2d69d089bdae2c1dddf46504760b671073264f22
Hardik Patel <h.patel290695@gmail.com>
2024-09-04 23:43:26 -0400

    Change in Login Page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { auth } from "../firbase-config";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
@@ -41,14 +42,30 @@ const Login = () => {
     }
 
     return (
-        <div>
-            <form onSubmit={login}>
-                <h1>Login</h1>
-                <input type="email" placeholder="Enter your email" onChange={(e) => { setEmail(e.target.value) }} />
-                <input type="password" placeholder="Enter your password" onChange={(e) => { setPassword(e.target.value) }} />
-                <button type="submit">Create Account</button>
+        <div className="flex justify-center">
+            <form className="flex flex-col space-y-4 p-6" onSubmit={login}>
+                <h1 className="text-3xl text-center font-playfair">Login</h1>
+                <input
+                    className="p-2 border border-gray-300 rounded"
+                    type="email"
+                    placeholder="Enter your email"
+                    onChange={(e) => { setEmail(e.target.value) }}
+                />
+                <input
+                    className="p-2 border border-gray-300 rounded"
+                    type="password"
+                    placeholder="Enter your password"
+                    onChange={(e) => { setPassword(e.target.value) }}
+                />
+                <button
+                    className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                    type="submit"
+                >
+                    Create Account
+                </button>
+                <p>Not have an account? <Link to="/">Register</Link></p>
             </form>
-            {user ? (
+            {/* {user ? (
                 <div>
                     <p>Welcome {user.email}</p>
                     <button onClick={logout}>Logout</button>
@@ -57,7 +74,7 @@ const Login = () => {
                 <div>
                     <p>You are not logged in</p>
                 </div>
-            )}
+            )} */}
         </div>
     )
 };
