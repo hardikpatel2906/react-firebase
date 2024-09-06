ba5945380c260d0296eedcdeeb0ea9fe338efc34
Hardik Patel <h.patel290695@gmail.com>
2024-09-05 22:22:33 -0400

    Change in Login Page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
-  const secretKey = process.env.REACT_APP_API_KEY;
-  console.log(secretKey);
-
-//   FB_API_KEY=AIzaSyD7q2YMWaSF1l-SngaUbXk4tK12XoEzloE
-// FB_AUTH_DOMAIN=react-auth-c5127.firebaseapp.com
-// FB_PROJECT_ID=react-auth-c5127
-// FB_STORAGE_BUCKET=react-auth-c5127.appspot.com
-// FB_MESSAGING_SENDER_ID=150562783230
-// FB_APP_ID=1:150562783230:web:8e16669fc8a08878ad4aaf
   return (
     <BrowserRouter>
       <div className="bg-red-50 min-h-screen">
